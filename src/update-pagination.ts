4b5d50f47bb87637c596d20237bd389890ddc03b
A Cottrill <a.rcottrill521@gmail.com>
2024-09-16 15:45:43 -0400

    fix: proper trailing slashes

--- src/update-pagination.ts
+++ src/update-pagination.ts
@@ -19,17 +19,20 @@ export const content: Section[] = [
   {
     title: 'sidebar.introduction',
     contents: [
-      { href: '/getting-started', title: 'sidebar.gettingStarted' },
-      { href: '/FAQ', title: 'sidebar.faq' },
-      { href: '/security', title: 'sidebar.report_vulnerability' }
+      { href: '/getting-started/', title: 'sidebar.gettingStarted' },
+      { href: '/faq/', title: 'sidebar.faq' },
+      { href: '/security/', title: 'sidebar.report_vulnerability' }
     ]
   },
   {
     title: 'sidebar.translation_contribution',
     contents: [
-      { href: '/how-to-translate-files', title: 'sidebar.translate_resources' },
       {
-        href: '/how-to-proofread-files',
+        href: '/how-to-translate-files/',
+        title: 'sidebar.translate_resources'
+      },
+      {
+        href: '/how-to-proofread-files/',
         title: 'sidebar.proofread_translations'
       }
     ]
@@ -38,53 +41,53 @@ export const content: Section[] = [
     title: 'sidebar.code_contribution',
     contents: [
       {
-        href: '/how-to-setup-freecodecamp-locally',
+        href: 'how-to-setup-freecodecamp-locally/',
         title: 'sidebar.setup_freecodecamp'
       },
-      { href: '/codebase-best-practices', title: 'sidebar.best_practices' },
+      { href: 'codebase-best-practices/', title: 'sidebar.best_practices' },
       {
-        href: '/how-to-contribute-to-the-codebase',
+        href: 'how-to-contribute-to-the-codebase/',
         title: 'sidebar.work_on_codebase'
       },
       {
-        href: '/how-to-work-on-coding-challenges',
+        href: 'how-to-work-on-coding-challenges/',
         title: 'sidebar.work_on_coding_challenges'
       },
-      { href: '/curriculum-help', title: 'sidebar.use_the_curriculum_helpers' },
+      { href: 'curriculum-help/', title: 'sidebar.use_the_curriculum_helpers' },
       {
-        href: '/how-to-work-on-the-component-library',
+        href: 'how-to-work-on-the-component-library/',
         title: 'sidebar.work_on_component_library'
       },
       {
-        href: '/how-to-work-on-practice-projects',
+        href: 'how-to-work-on-practice-projects/',
         title: 'sidebar.work_on_practice_projects'
       },
       {
-        href: '/how-to-setup-freecodecamp-mobile-app-locally',
+        href: 'how-to-setup-freecodecamp-mobile-app-locally/',
         title: 'sidebar.work_on_mobile_app'
       },
       {
-        href: '/how-to-work-on-tutorials-that-use-coderoad',
+        href: 'how-to-work-on-tutorials-that-use-coderoad/',
         title: 'sidebar.coderoad'
       },
       {
-        href: '/how-to-work-on-localized-client-webapp',
+        href: 'how-to-work-on-localized-client-webapp/',
         title: 'sidebar.work_on_webapp'
       },
       {
-        href: '/how-to-add-playwright-tests',
+        href: 'how-to-add-playwright-tests/',
         title: 'sidebar.work_on_playwright_tests'
       },
       {
-        href: '/how-to-help-with-video-challenges',
+        href: 'how-to-help-with-video-challenges/',
         title: 'sidebar.work_on_video_challenges'
       },
       {
-        href: '/how-to-work-on-the-docs-theme',
+        href: 'how-to-work-on-the-docs-theme/',
         title: 'sidebar.work_on_documentation'
       },
       {
-        href: '/how-to-open-a-pull-request',
+        href: 'how-to-open-a-pull-request/',
         title: 'sidebar.open_a_pull_request'
       }
     ]
@@ -92,23 +95,23 @@ export const content: Section[] = [
   {
     title: 'sidebar.additional_guides',
     contents: [
-      { href: '/curriculum-file-structure', title: 'sidebar.file_structure' },
+      { href: 'curriculum-file-structure/', title: 'sidebar.file_structure' },
       {
-        href: '/how-to-catch-outgoing-emails-locally',
+        href: 'how-to-catch-outgoing-emails-locally/',
         title: 'sidebar.debug_outgoing_emails_locally'
       },
-      { href: '/how-to-setup-wsl', title: 'sidebar.setup_fcc_on_wsl' },
+      { href: 'how-to-setup-wsl/', title: 'sidebar.setup_fcc_on_wsl' },
       {
-        href: '/how-to-use-docker-on-windows-home',
+        href: 'how-to-use-docker-on-windows-home/',
         title: 'sidebar.docker_on_windows_home'
       },
-      { href: '/user-token-workflow', title: 'sidebar.user_token_workflow' },
+      { href: 'user-token-workflow/', title: 'sidebar.user_token_workflow' },
       {
-        href: '/troubleshooting-development-issues',
+        href: 'troubleshooting-development-issues/',
         title: 'sidebar.troubleshooting_development_issues'
       },
       {
-        href: '/authors-analytics-manual',
+        href: 'authors-analytics-manual/',
         title: 'sidebar.authors_analytics_manual'
       }
     ]
@@ -117,19 +120,19 @@ export const content: Section[] = [
     title: 'sidebar.flight_manuals',
     addSeparator: true,
     contents: [
-      { href: '/moderator-handbook', title: 'sidebar.moderator_handbook' },
-      { href: '/reply-templates', title: 'sidebar.reply_templates' },
+      { href: 'moderator-handbook/', title: 'sidebar.moderator_handbook' },
+      { href: 'reply-templates/', title: 'sidebar.reply_templates' },
       {
-        href: '/language-lead-handbook',
+        href: 'language-lead-handbook/',
         title: 'sidebar.language_lead_handbook'
       },
-      { href: '/devops', title: 'sidebar.devops_handbook' },
+      { href: 'devops/', title: 'sidebar.devops_handbook' },
       {
-        href: '/courses-vscode-extension',
+        href: 'courses-vscode-extension/',
         title: 'sidebar.courses_vscode_extension'
       },
       {
-        href: '/how-to-enable-new-languages',
+        href: 'how-to-enable-new-languages/',
         title: 'sidebar.enable_new_language'
       }
     ]
