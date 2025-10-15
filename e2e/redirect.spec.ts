5b7aa7a61f165292f845280858d369d23daf1c97
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2025-10-15 17:11:30 +0200

    fix(test): await page redirect

--- e2e/redirect.spec.ts
+++ e2e/redirect.spec.ts
@@ -3,11 +3,11 @@ import { test, expect } from '@playwright/test';
 test.describe('# redirection', () => {
   test('redirects #/ to /intro', async ({ page }) => {
     await page.goto('#/');
-    expect(page).toHaveURL('/intro/');
+    await expect(page).toHaveURL('/intro/');
   });
 
   test('redirects /#/page?id=anchor to /page/#anchor', async ({ page }) => {
     await page.goto('/#/page?id=anchor');
-    expect(page).toHaveURL('/page/#anchor');
+    await expect(page).toHaveURL('/page/#anchor');
   });
 });
