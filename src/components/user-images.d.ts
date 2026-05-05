08c14c05ec88c11bc2fcaa7258def5de43a7856c
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2026-05-05 11:13:42 +0200

    fix: link logo to /intro, since / redirects there

---
+++ src/components/user-images.d.ts
@@ -0,0 +1,16 @@
+// Used by starlight's SiteTitle component, but not declared.
+declare module 'virtual:starlight/user-images' {
+  const logos: {
+    light: {
+      src: string;
+      width: number;
+      height: number;
+    };
+    dark: {
+      src: string;
+      width: number;
+      height: number;
+    };
+  };
+  export { logos };
+}
